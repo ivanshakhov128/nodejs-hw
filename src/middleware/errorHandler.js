f5086914f0f7e359d2f8d6a8d1f9362b0ebaf79f
Ivan Shakhov <ivanshahov04@gmail.com>
2026-01-26 21:20:28 +0200

    notes CRUD with mongoose_3

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,10 +1,10 @@
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
   let status = 500;
   let message = 'Server error';
 
-  if (createHttpError.isHttpError(err)) {
+  if (err instanceof HttpError) {
     status = err.status;
     message = err.message || 'Http Error';
   } else if (err instanceof Error) {
